a951182cbf09c5f8820b2ac7510c5bf09fd6cd45
Pablo Fino <finopablo@gmail.com>
2016-10-03 09:07:18 -0300

    Second commit

--- requesthandler.js
+++ requesthandler.js
@@ -1,25 +1,19 @@
 var exec = require("child_process").exec;
 function start(response) {
   console.log("Manipulador de petición 'start' fue llamado.");
-
-  var async_function = function(response, callback){
-    process.nextTick(function(){
-      function sleep(milliSeconds) {
-          var startTime = new Date().getTime();
-          while (new Date().getTime() < startTime + milliSeconds);
-      }
-      sleep(10000);
-      callback(response);
-    });
-  };
-
-  async_function(response, function(response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("hola start");
-    response.end();
-  });
-  }
-
+  var endTime = new Date().getTime() + 1000 * 10;
+  (function run() {
+          process.nextTick(function() {
+              if (new Date().getTime() < endTime) {
+                  run();
+              } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.write("hola start");
+                response.end();
+              }
+          });
+      })()
+}
 function up(response) {
   console.log("Manipulador de petición 'up' fue llamado.");
   response.writeHead(200, {"Content-Type": "text/html"});
